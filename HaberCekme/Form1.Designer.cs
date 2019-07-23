a6cd2337fa23f754a8d11ac32f7713880bbd770f
kamile ener <enerkamile@hotmail.com>
2019-07-23 13:55:53 +0300

    değişiklik

--- HaberCekme/Form1.Designer.cs
+++ HaberCekme/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -58,7 +59,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.Location = new System.Drawing.Point(33, 142);
+            this.listBox1.Location = new System.Drawing.Point(33, 121);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(555, 212);
             this.listBox1.TabIndex = 2;
@@ -72,11 +73,22 @@
             this.comboBox2.TabIndex = 4;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 49);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(683, 371);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
@@ -95,6 +107,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ WebServis/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebServis.Models;
+
+namespace WebServis.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        ///// <summary>
+        ///// GET api/GetTestResponse
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet]
+        //public TestResponse GetTestResponse()
+        //{
+        //    return new TestResponse {
+        //        tamSayisi = 55,
+        //        test ="jdıwkdhwdn" };
+        //}
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public TestResponse Get(int id)
+        {
+            return new TestResponse
+            {
+                        tamSayisi = 55,
+                        test ="jdıwkdhwdn" };
+            }
+
+        // POST api/values
+            public void Post([FromBody]string value)
+        {
+
+            //buraya yazıcaksınnn
+
+
+
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
